3c662944486f466c02dbf6db89c79d5c43c22634
arvind0251 <160855021+arvind0251@users.noreply.github.com>
2025-01-17 19:42:27 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,6 +1,6 @@
 import asyncio
 from pyrogram import Client, filters
-from pyrogram.types: InlineKeyboardMarkup, InlineKeyboardButton
+from pyrogram.types import InlineKeyboardMarkup, InlineKeyboardButton
 from config import API_ID, API_HASH, BOT_TOKEN
 
 # Initialize bot client
